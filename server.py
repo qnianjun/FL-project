fd86924e1e34842ced68145ff18d86bfff922105
qnianjun <qin.nian.jun.taichung@gmail.com>
2026-09-08 20:45:56 +0800

    Complete baseline FedAvg experiment

--- server.py
+++ server.py
@@ -1,25 +1,24 @@
 import flwr as fl
+import csv
 
+def weighted_average(metrics):
+    """根據每個 Client 的資料量計算加權平均 Accuracy"""
 
-# =========================
-# 1. Evaluation
-# =========================
+    accuracies = [
+        num_examples * metrics["accuracy"]
+        for num_examples, metrics in metrics
+    ]
 
-def evaluate(
-    server_round,
-    parameters,
-    config
-):
+    examples = [
+        num_examples
+        for num_examples, _ in metrics
+    ]
 
-    return 0.0, {
-        "accuracy": 0.0
+    return {
+        "accuracy": sum(accuracies) / sum(examples)
     }
 
-
-# =========================
-# 2. FedAvg Strategy
-# =========================
-
+# build FebAvg Strategy
 strategy = fl.server.strategy.FedAvg(
     fraction_fit=1.0,
     fraction_evaluate=1.0,
@@ -28,17 +27,14 @@ strategy = fl.server.strategy.FedAvg(
     min_evaluate_clients=5,
     min_available_clients=5,
 
-    evaluate_fn=evaluate,
+    evaluate_metrics_aggregation_fn=weighted_average,
 )
 
 
-# =========================
-# 3. Start Server
-# =========================
-
 if __name__ == "__main__":
 
-    fl.server.start_server(
+    # start Server
+    history = fl.server.start_server(
         server_address="127.0.0.1:8080",
 
         config=fl.server.ServerConfig(
@@ -47,3 +43,45 @@ if __name__ == "__main__":
 
         strategy=strategy
     )
+
+    # =========================
+    # save result
+    # =========================
+
+    with open("results.csv", "w", newline="") as f:
+
+        writer = csv.writer(f)
+
+        # CSV 第一列
+        writer.writerow([
+            "round",
+            "loss",
+            "accuracy"
+        ])
+
+        # 取得 Loss
+        loss_history = dict(
+            history.losses_distributed
+        )
+
+        # 取得 Accuracy
+        acc_history = dict(
+            history.metrics_distributed["accuracy"]
+        )
+
+        # 找出所有 Round
+        rounds = sorted(
+            set(loss_history.keys()) |
+            set(acc_history.keys())
+        )
+
+        # 寫入每一輪
+        for r in rounds:
+
+            writer.writerow([
+                r,
+                loss_history.get(r, ""),
+                acc_history.get(r, "")
+            ])
+
+    print("Results saved to results.csv")
