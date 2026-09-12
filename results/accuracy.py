7163d63a6f306f7ffb3eaba145b8a3fb545fd47b
qnianjun <qin.nian.jun.taichung@gmail.com>
2026-09-12 18:47:14 +0800

    result of every poisoning scale

--- results/accuracy.py
+++ results/accuracy.py
@@ -3,31 +3,64 @@ import pandas as pd
 
 
 #read file
-iid=pd.read_csv("baseline_iid.csv")
-noniid=pd.read_csv("noniid.csv")
+poisoning_1=pd.read_csv("1_poisoning.csv")
+poisoning_2=pd.read_csv("2_poisoning.csv")
+poisoning_5=pd.read_csv("5_poisoning.csv")
+poisoning_10=pd.read_csv("10_poisoning.csv")
+poisoning_20=pd.read_csv("20_poisoning.csv")
+poisoning_50=pd.read_csv("50_poisoning.csv")
 
 #draw
 plt.plot(
-    iid["round"],
-    iid["accuracy"] *100,
+    poisoning_1["round"],
+    poisoning_1["accuracy"] *100,
     marker="o",
-    label="IID"
+    label="1"
 )
 
 plt.plot(
-    noniid["round"],
-    noniid["accuracy"] *100,
+    poisoning_2["round"],
+    poisoning_2["accuracy"] *100,
     marker="o",
-    label="Non-IID"
+    label="2"
 )
+
+plt.plot(
+    poisoning_5["round"],
+    poisoning_5["accuracy"] *100,
+    marker="o",
+    label="5"
+)
+
+plt.plot(
+    poisoning_10["round"],
+    poisoning_10["accuracy"] *100,
+    marker="o",
+    label="10"
+)
+
+plt.plot(
+    poisoning_20["round"],
+    poisoning_20["accuracy"] *100,
+    marker="o",
+    label="20"
+)
+
+plt.plot(
+    poisoning_50["round"],
+    poisoning_50["accuracy"] *100,
+    marker="o",
+    label="50"
+)
+
 
 plt.xlabel("Round")
 plt.ylabel("Accuracy (%)")
-plt.title("IID vs Non-IID Accuracy")
+plt.title("Accuracy of every scale")
 plt.legend()
 plt.grid(True)
 
 plt.tight_layout()
-plt.savefig("iid_vs_noniid.png",dpi=300)
+plt.savefig("poisoning.png",dpi=300)
 
 plt.show()
