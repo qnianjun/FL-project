f9dedda52a7f7bb332091668f9b5f6a8eafba7a9
qnianjun <qin.nian.jun.taichung@gmail.com>
2026-09-13 16:52:42 +0800

    result of every poisoning clients

--- results/accuracy.py
+++ results/accuracy.py
@@ -3,60 +3,53 @@ import pandas as pd
 
 
 #read file
-poisoning_1=pd.read_csv("1_poisoning.csv")
-poisoning_2=pd.read_csv("2_poisoning.csv")
-poisoning_5=pd.read_csv("5_poisoning.csv")
-poisoning_10=pd.read_csv("10_poisoning.csv")
-poisoning_20=pd.read_csv("20_poisoning.csv")
-poisoning_50=pd.read_csv("50_poisoning.csv")
+clients_1=pd.read_csv("1_poisoning_clients.csv")
+clients_2=pd.read_csv("2_poisoning_clients.csv")
+clients_3=pd.read_csv("3_poisoning_clients.csv")
+clients_4=pd.read_csv("4_poisoning_clients.csv")
+clients_5=pd.read_csv("5_poisoning_clients.csv")
 
 #draw
 plt.plot(
-    poisoning_1["round"],
-    poisoning_1["accuracy"] *100,
+    clients_1["round"],
+    clients_1["accuracy"] *100,
     marker="o",
     label="1"
 )
 
 plt.plot(
-    poisoning_2["round"],
-    poisoning_2["accuracy"] *100,
+    clients_2["round"],
+    clients_2["accuracy"] *100,
     marker="o",
     label="2"
 )
 
 plt.plot(
-    poisoning_5["round"],
-    poisoning_5["accuracy"] *100,
+    clients_3["round"],
+    clients_3["accuracy"] *100,
     marker="o",
     label="5"
 )
 
 plt.plot(
-    poisoning_10["round"],
-    poisoning_10["accuracy"] *100,
+    clients_4["round"],
+    clients_4["accuracy"] *100,
     marker="o",
     label="10"
 )
 
 plt.plot(
-    poisoning_20["round"],
-    poisoning_20["accuracy"] *100,
+    clients_5["round"],
+    clients_5["accuracy"] *100,
     marker="o",
     label="20"
 )
 
-plt.plot(
-    poisoning_50["round"],
-    poisoning_50["accuracy"] *100,
-    marker="o",
-    label="50"
-)
 
 plt.xlabel("Round")
 plt.ylabel("Accuracy (%)")
 plt.ylim(0, 100)
-plt.title("Accuracy by Poisoning Scale")
+plt.title("Accuracy by Poisoning client's numbers")
 
 plt.legend()
 plt.grid(True)
