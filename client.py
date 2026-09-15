bb38da0f8e870e6d151acf1819a7d6d66b14fa12
hanbao-bao <liberku@gmail.com>
2026-09-15 19:43:58 +0800

    67

--- client.py
+++ client.py
@@ -133,8 +133,8 @@ class FlowerClient(fl.client.NumPyClient):
             0: [0, 1,2],
             1: [2, 3,4],
             2: [4, 5,6],
-            3: [6, 7,8],
-            4: [8, 9,0]
+            3: [6,7,8],
+            4: [8,9,0]
         }
 
         my_labels = client_labels[int(cid)]
