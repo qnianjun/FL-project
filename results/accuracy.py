a9c1a08fa424f75a28fe14884e3a5a0fd609cb35
qnianjun <qin.nian.jun.taichung@gmail.com>
2026-09-15 20:18:04 +0800

    the result of Dirichlet Non-IID

--- results/accuracy.py
+++ results/accuracy.py
@@ -3,31 +3,38 @@ import pandas as pd
 
 
 #read file
-iid=pd.read_csv("baseline_iid.csv")
-noniid=pd.read_csv("noniid.csv")
-
+light=pd.read_csv("light.csv")
+mid=pd.read_csv("mid.csv")
+serious=pd.read_csv("serious.csv")
 #draw
 plt.plot(
-    iid["round"],
-    iid["accuracy"] *100,
+    light["round"],
+    light["accuracy"] *100,
     marker="o",
-    label="IID"
+    label="light"
 )
 
 plt.plot(
-    noniid["round"],
-    noniid["accuracy"] *100,
+    mid["round"],
+    mid["accuracy"] *100,
     marker="o",
-    label="Non-IID"
+    label="mid"
+)
+
+plt.plot(
+    serious["round"],
+    serious["accuracy"] *100,
+    marker="o",
+    label="serious"
 )
 
 plt.xlabel("Round")
 plt.ylabel("Accuracy (%)")
-plt.title("IID vs Non-IID Accuracy")
+plt.title("Dirichlet Non-IID Accuracy")
 plt.legend()
 plt.grid(True)
 
 plt.tight_layout()
-plt.savefig("iid_vs_noniid.png",dpi=300)
+plt.savefig("Dirichlet_Non_IID.png",dpi=300)
 
 plt.show()
